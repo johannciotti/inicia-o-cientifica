3160124dead96b5b261a2d1104e4855789f570d0
johannciotti <51494606+johannciotti@users.noreply.github.com>
2019-06-27 19:53:30 -0300

    Update rodarbash.sh

--- rodarbash.sh
+++ rodarbash.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-for i in {1..8}; do 
+for i in {1..2}; do 
   gcc -O3 -o x$i.x IandF_johann_g4_eletric_bash.c -lm
   echo 
 done
+#!/bin/bash
 
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#
 
-for i in {1..8}; do 
-   echo $ i | ./x$i.x & 
-   echo 
-   sleep 1
-   rm x$i.x 
+source /opt/intel/Compiler/11.1/064/bin/iccvars.sh intel64
+icc <code.c> -o ./x$i.x &
+ ./x$i.x &
+ 
 done
 
