84f02188b49b8baa169ca8ed695506c725bb4e83
johannciotti <51494606+johannciotti@users.noreply.github.com>
2019-06-27 22:16:02 -0300

    Update rodarbash.sh

--- rodarbash.sh
+++ rodarbash.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
-for i in {1..2}; do 
-  gcc -O3 -o x$i.x IandF_johann_g4_eletric_bash.c -lm
-  echo 
-done
-#!/bin/bash
 
+
+
+#!/bin/bash
+#
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
 #
 
 source /opt/intel/Compiler/11.1/064/bin/iccvars.sh intel64
-icc <code.c> -o ./x$i.x &
- ./x$i.x &
- 
-done
-
+icc <code.c> -o  	IandF_johann_g4_eletric_bash1.c
+ ./ 	IandF_johann_g4_eletric_bash1.c
+ icc <code.c> -o  	IandF_johann_g4_eletric_bash2.c
+ ./ 	IandF_johann_g4_eletric_bash2.c
+ icc <code.c> -o  	IandF_johann_g4_eletric_bash3.c
+ ./ 	IandF_johann_g4_eletric_bash3.c
+ icc <code.c> -o  	IandF_johann_g4_eletric_bash4.c
+ ./ 	IandF_johann_g4_eletric_bash4.c
+ icc <code.c> -o  	IandF_johann_g4_eletric_bash5.c
+ ./ 	IandF_johann_g4_eletric_bash5.c
+ icc <code.c> -o  	IandF_johann_g4_eletric_bash6.c
+ ./ 	IandF_johann_g4_eletric_bash6.c
+ icc <code.c> -o  	IandF_johann_g4_eletric_bash7.c
+ ./ 	IandF_johann_g4_eletric_bash7.c
+ icc <code.c> -o  	IandF_johann_g4_eletric_bash8.c
+ ./ 	IandF_johann_g4_eletric_bash8.c
+ end
