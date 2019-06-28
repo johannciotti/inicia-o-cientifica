2cc34c51ddc8276729594be07f3f68ecc8430972
johannciotti <51494606+johannciotti@users.noreply.github.com>
2019-06-27 23:55:47 -0300

    Update rodarbash.sh

--- rodarbash.sh
+++ rodarbash.sh
@@ -7,18 +7,4 @@
 
 icc <code.c> -o  	IandF_johann_g4_eletric_bash1.c
  ./ 	IandF_johann_g4_eletric_bash1.c
- icc <code.c> -o  	IandF_johann_g4_eletric_bash2.c
- ./ 	IandF_johann_g4_eletric_bash2.c
- icc <code.c> -o  	IandF_johann_g4_eletric_bash3.c
- ./ 	IandF_johann_g4_eletric_bash3.c
- icc <code.c> -o  	IandF_johann_g4_eletric_bash4.c
- ./ 	IandF_johann_g4_eletric_bash4.c
- icc <code.c> -o  	IandF_johann_g4_eletric_bash5.c
- ./ 	IandF_johann_g4_eletric_bash5.c
- icc <code.c> -o  	IandF_johann_g4_eletric_bash6.c
- ./ 	IandF_johann_g4_eletric_bash6.c
- icc <code.c> -o  	IandF_johann_g4_eletric_bash7.c
- ./ 	IandF_johann_g4_eletric_bash7.c
- icc <code.c> -o  	IandF_johann_g4_eletric_bash8.c
- ./ 	IandF_johann_g4_eletric_bash8.c
  
