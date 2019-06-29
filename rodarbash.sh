7ae006d68687268ecf7c570ec3414a2d27cfe3c7
johannciotti <51494606+johannciotti@users.noreply.github.com>
2019-06-29 12:26:40 -0300

    Update rodarbash.sh

--- rodarbash.sh
+++ rodarbash.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-#
-#$ -cwd
-#$ -j y
-#$ -S /bin/bash
-#
 
-icc <code.c> -o  	IandF_johann_g4_eletric_bash1.c
- ./ 	IandF_johann_g4_eletric_bash1.c
+for i in {1..8}; do 
+  gcc -O3 -o x$i.x IandF_johann_g4_eletric_bash.c -lm
+  echo 
+done
+
+for i in {1..8}; do 
+   echo $i | srun -n1 ./x$i.x & 
+   echo 
+   sleep 1
+   rm x$i.x 
+done
+
+
  
